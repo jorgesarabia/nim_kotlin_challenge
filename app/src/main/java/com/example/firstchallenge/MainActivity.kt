6a1fb174a075e962f0e629e0189d9a6c8471e740
jorgesarabia <jorge@sarabiajor.ge>
2023-11-04 14:34:47 -0300

    add adjust resize

--- app/src/main/java/com/example/firstchallenge/MainActivity.kt
+++ app/src/main/java/com/example/firstchallenge/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.firstchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var email : EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener(View.OnClickListener {
+            if (email.text.toString() == "user" && password.text.toString() == "1234"){
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
     }
 }
